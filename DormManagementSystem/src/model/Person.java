30d1a5bb499bc0c3562da86fa09bb06340253b33
Jyvhan Earl Ponce <140694543+kuroi17@users.noreply.github.com>
2025-11-17 15:06:41 +0000

    added feature/class-person

---
+++ DormManagementSystem/src/model/Person.java
@@ -0,0 +1,64 @@
+package model;
+
+/**
+ * Abstract class representing a person in the dorm management system.
+ * @author Kuroi
+ * 
+ * // person may contain common attributes like name, address, contact number, email, etc.
+ */
+
+
+ public abstract class Person {
+    private String name;
+    private String email;
+    private String contactInfo;
+    private String address;
+
+
+
+    public Person(String name, String email, String contactInfo, String address) {
+    this.name = name;
+    this.email = email;
+    this.contactInfo = contactInfo;
+    this.address = address;
+ }
+
+
+ // gettters and setters special methods
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getContactInfo() {
+        return contactInfo;
+    }
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+ }
+
+ 
+ // ABSTRACTION: Abstract method for polymorphism
+    public abstract String displayInfo();
+    
+    // OPTIONAL: toString() for debugging (you can add this later!)
+    // @Override
+    // public String toString() {
+    //     return "Person{fullName='" + fullName + "', email='" + email + "'}";
+    // }
+}
